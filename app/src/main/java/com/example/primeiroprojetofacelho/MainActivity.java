84c097533fe7ed2ee5e99bd3074aa63bcf66acd4
teago83 <thyagodesouzapereira@gmail.com>
2020-09-02 11:16:14 -0300

    Leves mudanças.

--- app/src/main/java/com/example/primeiroprojetofacelho/MainActivity.java
+++ app/src/main/java/com/example/primeiroprojetofacelho/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.primeiroprojetofacelho;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import android.widget.Button;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void disable (View v)
+    {
+        //Log.d("success","Button successfully disabled!");
+
+        /*View myView = findViewById(R.id.button01);
+        View mySecondView = findViewById(R.id.button02);
+        myView.setEnabled(false);
+        mySecondView.setArrozComCarne("false");
+        Button button = (Button) myView;
+        button.setText("New Disabled");
+
+        if (mySecondView.arrozComCarne == "false")
+        {
+
+        }
+
+
+        v.setEnabled(false);
+        Button b = (Button) v;
+        b.setText("Disabled");
+        */
+    }
 }
+
