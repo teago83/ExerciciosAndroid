4083a77abf4f32c30b6e63975598b7a3df966bad
teago83 <thyago.pereira@faculdadecesusc.edu.br>
2020-09-24 16:45:45 -0300

    Reformulações no código e adição de um 'botão negativo' para o usuário manter-se na tela onde ele já está. Os erros de português são propositais.

--- MyFirstAppOrSoIThink/app/src/main/java/com/example/myfirstapporsoithink/MainActivity.java
+++ MyFirstAppOrSoIThink/app/src/main/java/com/example/myfirstapporsoithink/MainActivity.java
@@ -1,35 +1,59 @@
 package com.example.myfirstapporsoithink;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText InputNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        InputNome = findViewById(R.id.YourName);
+        //Transição para a segunda tela
         Button TransitionButton = (Button) findViewById(R.id.SecondScreenButton);
         TransitionButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v){
-
-                String nome = InputNome.getText().toString();
-
+            public void onClick(View v){
                 Intent IntentUe = new Intent(MainActivity.this, SecondActivity.class);
-
-                IntentUe.putExtra("Nome", nome);
                 startActivity(IntentUe);
-                finish();
             }
         });
+
+        //Ato de manter-se na primeira tela
+        Button NenButton = (Button) findViewById(R.id.Nen);
+        NenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FicarNaPrimeiraTela(v);
+            }
+        });
+    }
+
+    public void FicarNaPrimeiraTela(View v){
+        AlertDialog.Builder Alerta = new AlertDialog.Builder(this);
+        Alerta.setTitle("pergunta fundamental");
+        Alerta.setMessage("ae, vc n queria ir pra sigunda tela de novo, ou quiria!!!???? >:(");
+        Alerta.setPositiveButton("nao maluco", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "acho bao", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Alerta.setNegativeButton("claro q nao", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "to di oio ein ¬¬", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Alerta.create().show();
     }
 }
--- MyFirstAppOrSoIThink/app/src/main/java/com/example/myfirstapporsoithink/SecondActivity.java
+++ MyFirstAppOrSoIThink/app/src/main/java/com/example/myfirstapporsoithink/SecondActivity.java
@@ -1,35 +1,59 @@
 package com.example.myfirstapporsoithink;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
-    private EditText InputSobrenome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        InputSobrenome = findViewById(R.id.YourLastName);
+        //Transição para a primeira tela
         Button TransitionButton = (Button) findViewById(R.id.MainScreenButton);
         TransitionButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v){
-
-                String sobrenome = InputSobrenome.getText().toString();
-
+            public void onClick(View v) {
                 Intent IntentUe = new Intent(SecondActivity.this, MainActivity.class);
-
-                IntentUe.putExtra("Sobrenome", sobrenome);
                 startActivity(IntentUe);
-                finish();
             }
         });
+
+        //Ato de manter-se na segunda tela
+        Button NenButton = (Button) findViewById(R.id.Nen);
+        NenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FicarNaSegundaTela(v);
+            }
+        });
+    }
+
+    public void FicarNaSegundaTela(View v){
+        AlertDialog.Builder Alerta = new AlertDialog.Builder(this);
+        Alerta.setTitle("pergunta mto importante");
+        Alerta.setMessage("ae, tu n quer ir pra primeira tela de novo, neh????");
+        Alerta.setPositiveButton("nao", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(SecondActivity.this, "imaginei msm", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Alerta.setNegativeButton("outro nao", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(SecondActivity.this, "po, concordo plenament", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Alerta.create().show();
     }
 }
