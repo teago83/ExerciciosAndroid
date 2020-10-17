3ee99acc408f7af0a673916f35c646fb09636500
teago83 <thyago.pereira@faculdadecesusc.edu.br>
2020-10-16 21:49:26 -0300

    Base para a atividade de múltiplos layouts (finalmente) pronta.

---
+++ MultipleLayouts/app/src/main/java/com/example/multiplelayouts/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.multiplelayouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button WryyyButton = (Button) findViewById(R.id.wryybutton);
+        WryyyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LoadSecondScreen = new Intent (MainActivity.this, MainActivity2.class);
+                startActivity(LoadSecondScreen);
+            }
+        });
+
+    }
+}
