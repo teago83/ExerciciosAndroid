eae15383688eba01ad026b7080760482ee1150d4
teago83 <thyago.pereira@faculdadecesusc.edu.br>
2020-10-29 19:12:28 -0300

    App com SQlite finalizado.

--- Database/app/src/main/java/com/example/database/DatabaseHelper.java
+++ Database/app/src/main/java/com/example/database/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.example.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -8,12 +10,14 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "Student.db";
-    public static final String TABLE_NAME = "student_table";
+    public static final String DATABASE_NAME = "Hunter.db";
+    public static final String TABLE_NAME = "hunter_table";
     public static final String COL_1 = "ID";
-    public static final String COL_2 = "NAME";
-    public static final String COL_3 = "SURNAME";
-    public static final String COL_4 = "MARKS";
+    public static final String COL_2 = "HUNTERNAME";
+    public static final String COL_3 = "HR";
+    public static final String COL_4 = "MR";
+    public static final String COL_5 = "FAVOURITE_WEAPON";
+    public static final String COL_6 = "FAVOURITE_MONSTER";
 
 
     public DatabaseHelper(Context context) {
@@ -22,11 +26,57 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("create table " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, HUNTERNAME STRING, HR INTEGER, MR INTEGER, FAVOURITE_WEAPON STRING, FAVOURITE_MONSTER STRING)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + TABLE_NAME);
+        onCreate(db);
+    }
 
+    public boolean InsertData(String Name, String Hr, String Mr, String Weapon, String Monster){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues cv = new ContentValues();
+        cv.put(COL_2, Name);
+        cv.put(COL_3, Hr);
+        cv.put(COL_4, Mr);
+        cv.put(COL_5, Weapon);
+        cv.put(COL_6, Monster);
+
+        long Result = db.insert(TABLE_NAME, null, cv);
+
+        if (Result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public Cursor ShowData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor Result = db.rawQuery("select * from " + TABLE_NAME, null);
+        return Result;
+    }
+
+    public boolean UpdateData(String Id, String Name, String Hr, String Mr, String Weapon, String Monster) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues cv = new ContentValues();
+        cv.put(COL_1, Id);
+        cv.put(COL_2, Name);
+        cv.put(COL_3, Hr);
+        cv.put(COL_4, Mr);
+        cv.put(COL_5, Weapon);
+        cv.put(COL_6, Monster);
+
+        db.update(TABLE_NAME, cv, "ID = ?", new String[] { Id });
+
+        return true;
+    }
+
+    public Integer DeleteData(String Id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?", new String[] { Id });
     }
 }
--- Database/app/src/main/java/com/example/database/MainActivity.java
+++ Database/app/src/main/java/com/example/database/MainActivity.java
@@ -1,14 +1,140 @@
 package com.example.database;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper db;
+
+    EditText Id, Name, Hr, Mr, Weapon, Monster;
+
+    Button Add, View, Update, Delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = new DatabaseHelper(this);
+
+        Id = (EditText)findViewById(R.id.id);
+        Name = (EditText)findViewById(R.id.hunter_name);
+        Hr = (EditText)findViewById(R.id.hr);
+        Mr = (EditText)findViewById(R.id.mr);
+        Weapon = (EditText)findViewById(R.id.favourite_weapon);
+        Monster = (EditText)findViewById(R.id.favourite_monster);
+
+        Add = (Button)findViewById(R.id.add);
+        View = (Button)findViewById(R.id.view);
+        Update = (Button)findViewById(R.id.update);
+        Delete = (Button)findViewById(R.id.delete);
+
+        AddData();
+        ShowData();
+        UpdateData();
+        DeleteData();
     }
+
+    public void AddData() {
+        Add.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(android.view.View v) {
+                        boolean Result = db.InsertData(Name.getText().toString(),
+                                                       Hr.getText().toString(),
+                                                       Mr.getText().toString(),
+                                                       Weapon.getText().toString(),
+                                                       Monster.getText().toString());
+
+                        if (Result)
+                            Toast.makeText(MainActivity.this, "Data successfully inserted!", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Unable to insert data...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
+    public void ShowData() {
+        View.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(android.view.View v) {
+                        Cursor Result = db.ShowData();
+
+                        if (Result.getCount() == 0){
+                            ShowMessage("Error!", "No data was found!");
+                            return;
+                        }
+
+                        StringBuffer Buffer = new StringBuffer();
+                        while (Result.moveToNext()) {
+                            Buffer.append("ID: " + Result.getString(0) + "\n");
+                            Buffer.append("Hunter's Name: " + Result.getString(1) + "\n");
+                            Buffer.append("HR: " + Result.getString(2) + "\n");
+                            Buffer.append("MR: " + Result.getString(3) + "\n");
+                            Buffer.append("Favourite Weapon: " + Result.getString(4) + "\n");
+                            Buffer.append("Favourite Monster: " + Result.getString(5) + "\n");
+                        }
+
+                        ShowMessage("Data: ", Buffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void UpdateData() {
+        Update.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(android.view.View v) {
+                        boolean HasUpdated = db.UpdateData(Id.getText().toString(),
+                                                           Name.getText().toString(),
+                                                           Hr.getText().toString(),
+                                                           Mr.getText().toString(),
+                                                           Weapon.getText().toString(),
+                                                           Monster.getText().toString());
+
+                        if (HasUpdated)
+                            Toast.makeText(MainActivity.this, "Data successfully updated!", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Unable to update data...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
+    public void DeleteData() {
+        Delete.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(android.view.View v) {
+                        Integer DeletedRows = db.DeleteData(Id.getText().toString());
+
+                        if (DeletedRows > 0)
+                            Toast.makeText(MainActivity.this, "Data successfully deleted!", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(MainActivity.this, "Unable to delete data...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+
+    public void ShowMessage(String Title, String Message) {
+        AlertDialog.Builder Builder = new AlertDialog.Builder(this);
+        Builder.setCancelable(true);
+        Builder.setTitle(Title);
+        Builder.setMessage(Message);
+        Builder.show();
+    }
+
+
 }
